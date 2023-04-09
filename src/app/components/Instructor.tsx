3e252e11b064827c22651621ede33cb7022afd77
Hassan <github/HassanFarooq704>
2023-04-09 18:22:19 +0500

    completed

--- src/app/components/Instructor.tsx
+++ src/app/components/Instructor.tsx
@@ -13,10 +13,11 @@ export default function Instructor() {
                 </Center>
                 <RevealWrapper origin='top' delay={200} duration={1000} distance='500px' reset={true}>
                     <Flex flexDir='row' pt={{ lg: '20px', base: '10px' }} display={{ lg: 'flex', base: 'grid' }} gap={{ lg: '0', base: '20px' }}>
+                     
                         {data.map((ins) => (
-                            <Box>
+                            <Box key={ins.id}>
                                 <Center>
-                                    <Image borderRadius='full' width='80px' height='80px' src={ins.src} />
+                                    <Image borderRadius='full' width='80px' height='80px' src={ins.src} alt='panaverse' />
                                 </Center>
                                 <Heading textAlign='center' size='md'>{ins.heading}</Heading>
                                 <Text size='sm' px={{ lg: '40px', base: '20px' }} textAlign='center'>{ins.text} </Text>
--- src/app/syllabus/page.tsx
+++ src/app/syllabus/page.tsx
@@ -2,7 +2,7 @@
 import { Box, Center, Container, Flex, Heading, Text, } from '@chakra-ui/react'
 import Image from 'next/image'
 import React from 'react'
-import { cards,core } from '../components/database'
+import { cards, core } from '../components/database'
 
 import Home from '../components/Home'
 import { RevealList, RevealWrapper } from 'next-reveal'
@@ -15,27 +15,27 @@ export default function page() {
     <Box pt='40px'>
       <Container maxW={1400} pb='40px'>
         <Center flexDir='column'>
-          <Heading size='2xl' >Course Syllabus</Heading>
-          <Text textAlign='center' px='150px' pt='20px'>The first three quarters are shared by all specialties and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a fifteen-month-long hybrid program that includes both onsite and online classes and is divided into five quarters of 13 weeks each</Text>
+          <Heading alignContent={{ base: 'left' }} size='2xl' >Course Syllabus</Heading>
+          <Text textAlign='center' px={{ lg: '150px', base: '20px' }} pt='20px'>The first three quarters are shared by all specialties and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a fifteen-month-long hybrid program that includes both onsite and online classes and is divided into five quarters of 13 weeks each</Text>
         </Center>
         <Center flexDir='column'>
           <Heading mt='30px' >Common In All</Heading>
-          <Text textAlign='center' px='150px' pt='20px'>Every participant of the program will start by completing the following three core courses:</Text>
+          <Text textAlign='center' px={{ lg: '150px', base: '20px' }} pt='20px'>Every participant of the program will start by completing the following three core courses:</Text>
         </Center>
         <Center>
-          <Flex gap='34px' mb='30px' pt='60px' justifyContent='center' >
+          <Flex gap='34px' mb='30px' pt='60px' justifyContent='center' flexDir={{ lg: 'row', base: 'column' }}>
             {core.map((elem) => (
-            <Box textAlign='center' p='30px' boxShadow='dark-lg' borderRadius='20px' width='320px' key={elem.id}>
-              <Heading textDecoration='underline' pb='20px'>{elem.heading}</Heading>
-              <Text>{elem.text}</Text>
-            </Box>
+              <Box textAlign='center' p='30px' boxShadow='dark-lg' borderRadius='20px' width='320px' key={elem.id}>
+                <Heading textDecoration='underline' pb='20px'>{elem.heading}</Heading>
+                <Text>{elem.text}</Text>
+              </Box>
             ))}
           </Flex>
         </Center>
 
         <Center flexDir='column'>
           <Heading mt='30px' >Specialized Tracks</Heading>
-          <Text textAlign='center' px='150px' pt='20px'>After completing the first two quarters the participants will select one or more specializations consisting of two courses each:</Text>
+          <Text textAlign='center' px={{ lg: '150px', base: '20px' }} pt='20px'>After completing the first two quarters the participants will select one or more specializations consisting of two courses each:</Text>
         </Center>
       </Container>
 
@@ -43,26 +43,33 @@ export default function page() {
       <Container maxW={1400} pt='50px' bgImage='https://www.apingenieria.com/wp-content/plugins/page-layout-builder/images/bg/geometricBGs43.jpg' bgSize='cover' bgAttachment='fixed'>
         {cards.map((elem) => (
           <Box key={elem.id}>
-            <Center flexDir='column'>
-              <Heading color='black'  >{elem.heading1}</Heading>
-              <Text fontWeight='medium' color='black' textAlign='center' px='150px' pt='20px'>{elem.text1}</Text>
-            </Center>
+            <RevealWrapper origin='left' delay={200} duration={2000} distance='500px' reset={true}>
+              <Center flexDir='column'>
+                <Heading color='black'  >{elem.heading1}</Heading>
+                <Text fontWeight='medium' color='black' textAlign='center' px={{ lg: '150px', base: '20px' }} pt='20px'>{elem.text1}</Text>
+              </Center>
+            </RevealWrapper>
             <Center>
-              <Flex gap='34px' mb='30px' pt='60px' justifyContent='center' >
-                <Box bgColor='white' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='20px' width='360px' >
-                  <Heading color='black' textDecoration='underline' pb='20px'>{elem.heading2}</Heading>
-                  <Text fontWeight='medium' color='black' px='12px'>{elem.text2}</Text>
-                </Box>
+              <Flex gap='34px' mb='30px' pt='60px' justifyContent='center' flexDir={{ lg: 'row', base: 'column' }}>
+                <RevealWrapper origin='left' delay={200} duration={2000} distance='500px' reset={true}>
+                  <Box bgColor='white' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='20px' width='360px' >
+                    <Heading color='black' textDecoration='underline' pb='20px'>{elem.heading2}</Heading>
+                    <Text fontWeight='medium' color='black' px='12px' >{elem.text2}</Text>
+                  </Box>
+                </RevealWrapper>
+                <RevealWrapper origin='right' delay={200} duration={2000} distance='500px' reset={true}>
                 <Box bgColor='white' textAlign='center' p='50px' boxShadow='dark-lg' borderRadius='20px' width='360px' >
                   <Heading color='black' textDecoration='underline' pb='20px'>{elem.heading3}</Heading>
                   <Text fontWeight='medium' color='black' px='12px'>{elem.text3}</Text>
                 </Box>
-              </Flex>
-            </Center>
+              </RevealWrapper>
+            </Flex>
+          </Center>
+
           </Box>
         ))}
-      </Container>
-    </Box>
+  </Container >
+    </Box >
 
   </>
   )
